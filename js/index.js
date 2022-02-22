7e3013b5161687c9d974c54a7fc9680a47819956
emanuellyfernandes <emanuellymbf22@gmail.com>
2022-02-22 06:20:16 -0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,43 @@
+new Glider(document.querySelector('.glider'), {
+  slidesToScroll: 1,
+  slidesToShow: 5.5,
+  draggable: true,
+  dots: '.dots',
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  }
+});
+
+new Glider(document.querySelector('.glider2'), {
+  slidesToScroll: 1,
+  slidesToShow: 5.5,
+  draggable: true,
+  dots: '.dots',
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  }
+});
+
+new Glider(document.querySelector('.glider3'), {
+  slidesToScroll: 1,
+  slidesToShow: 5.5,
+  draggable: true,
+  dots: '.dots',
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  }
+});
+
+new Glider(document.querySelector('.glider4'), {
+  slidesToScroll: 1,
+  slidesToShow: 5.5,
+  draggable: true,
+  dots: '.dots',
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  }
+});
